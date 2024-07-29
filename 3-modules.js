83827e0ad2cd257ba2af6080f2119b562df49257
Ray <iliyapopov805@gmail.com>
2024-07-29 19:11:16 +0400

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const names = require('./4-names');
+const hi = require('./5-utils');
+const data = require('./6-alternative');
+//console.log(data)
+require('./7-mind-grenade');
+//hi('Susan');
+//hi(names.peter);
+//hi(names.john);
---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'secret';
+
+const peter = 'Peter';
+const john = 'John';
+
+module.exports = {john, peter};
